e0a041d5aba114fb3fdcc66de8ca9f9095f26cd6
Aaron Tallman <aaronetallman@gmail.com>
2018-01-21 20:07:21 -0500

    Mush faster data processing
    Thanks to sets and their many virtues

--- codenames/data_collection.py
+++ codenames/data_collection.py
@@ -484,92 +484,102 @@ def compress_to_targets(targetlist,data,bigrams):
 
     sp = list(filter(lambda x: x.find(' ') > 0, targetlist))
     spw = []
+    tls=set(targetlist)
     for isp in sp:
         spw.extend(isp.split(' '))
+    tlsw=set(spw)
+    def add_bigram(temp,set):
+        for i in set:
+            lt=[temp.index(i)]
+            for i3 in range(temp.count(i)-1):
+                lt.append(temp.index(i,lt[len(lt)-1]))
+            for i2 in [temp[ii] for ii in lt]:
+                if i2 in tls:
+                # get prev and next
+                # save bigrams as numbers? sparseness precludes array or indicial storage
+    
+                    if temp.index(i2) == 0:
+                        a = 0
+                    else:
+                        a = temp[temp.index(i2) - 1]
+                    b = i2
+                    if len(temp) - temp.index(i2) == 1:
+                        c = 0
+                    else:
+                        c = temp[temp.index(i2) + 1]
+    #                if hits.get(b, 0) == 0:
+    #                    hits[b] = []
+                    if not a == 0:
+                        if not len(temp[temp.index(i2) - 1]) < 3:
+                            # we are not interested in clues shorter than 3 letters long
+                            #ab = '%i %i' % (a, b)
+                            if b in bigrams.keys():
+                                if a in bigrams[b].keys():
+                                    bigrams[b][a][0] += 1
+                                else:
+                                    bigrams[b][a] = [1,0]
+                            else:
+                                bigrams[b] = {a:[1,0]}
+    #                            hits[b].append(a)
+                    if not c == 0:
+                        if not len(temp[temp.index(i2) + 1]) < 3:
+                            #bc = '%i %i' % (b, c)
+                            if b in bigrams.keys():
+                                if c in bigrams[b].keys():
+                                    bigrams[b][c][1] += 1
+                                else:
+                                    bigrams[b][c] = [0,1]
+                            else:
+                                bigrams[b] = {c:[0,1]}
+    #                            hits[b].append(c)
+                elif i2 in tlsw:
+                    mod = spw.index(i2) % 2
+                    full = sp[spw.index(i2) // 2]
+                    if min(len(temp) - temp.index(i2) - 2 + mod, temp.index(i2) - mod) > -1:
+                        if temp[temp.index(i2) + 1 - 2 * mod] == spw[2 * (spw.index(i2) // 2) + 1 - mod]:
+                            if temp.index(i2) == mod:
+                                a = 0
+                            else:
+                                a = temp[temp.index(i2) - 1 - mod]
+                            b = full
+                            if len(temp) - temp.index(i2) == 2 - mod:
+                                c = 0
+                            else:
+                                c = temp[temp.index(i2) + 2 - mod]
+    #                        if hits.get(b, 0) == 0:
+    #                            hits[b] = []
+                            if not a == 0:
+                                if not len(temp[temp.index(i2) - 1 - mod]) < 3:
+                                    # we are not interested in clues shorter than 3 letters long
+                                    #ab = '%i %i' % (a, b)
+                                    if b in bigrams.keys():
+                                        if a in bigrams[b].keys():
+                                            bigrams[b][a][0] += 1
+                                        else:
+                                            bigrams[b][a] = [1,0]
+                                    else:
+                                        bigrams[b] = {a:[1,0]}
+    #                                    hits[b].append(a)
+                            if not c == 0:
+                                if not len(temp[temp.index(i2) + 2 - mod]) < 3:
+                                    #bc = '%i %i' % (b, c)
+                                    if b in bigrams.keys():
+                                        if c in bigrams[b].keys():
+                                            bigrams[b][c][1] += 1
+                                        else:
+                                            bigrams[b][c] = [0,1]
+                                    else:
+                                        bigrams[b] = {c:[0,1]}
+#                                    hits[b].append(c)
     # any codenames with spaces? clues can't contain spaces, so no need for support
     # beyond first iteration
-    
     d = open(data, 'r', encoding=ENC)
     for line in d:
         temp = line.replace('\n', '').split(' ')
-        for i2 in temp:
-            if i2 in targetlist:
-                # get prev and next
-                # save bigrams as numbers? sparseness precludes array or indicial storage
-
-                if temp.index(i2) == 0:
-                    a = 0
-                else:
-                    a = temp[temp.index(i2) - 1]
-                b = i2
-                if len(temp) - temp.index(i2) == 1:
-                    c = 0
-                else:
-                    c = temp[temp.index(i2) + 1]
-#                if hits.get(b, 0) == 0:
-#                    hits[b] = []
-                if not a == 0:
-                    if not len(temp[temp.index(i2) - 1]) < 3:
-                        # we are not interested in clues shorter than 3 letters long
-                        #ab = '%i %i' % (a, b)
-                        if b in bigrams.keys():
-                            if a in bigrams[b].keys():
-                                bigrams[b][a][0] += 1
-                            else:
-                                bigrams[b][a] = [1,0]
-                        else:
-                            bigrams[b] = {a:[1,0]}
-#                            hits[b].append(a)
-                if not c == 0:
-                    if not len(temp[temp.index(i2) + 1]) < 3:
-                        #bc = '%i %i' % (b, c)
-                        if b in bigrams.keys():
-                            if c in bigrams[b].keys():
-                                bigrams[b][c][1] += 1
-                            else:
-                                bigrams[b][c] = [0,1]
-                        else:
-                            bigrams[b] = {c:[0,1]}
-#                            hits[b].append(c)
-            elif i2 in spw:
-                mod = spw.index(i2) % 2
-                full = sp[spw.index(i2) // 2]
-                if min(len(temp) - temp.index(i2) - 2 + mod, temp.index(i2) - mod) > -1:
-                    if temp[temp.index(i2) + 1 - 2 * mod] == spw[2 * (spw.index(i2) // 2) + 1 - mod]:
-                        if temp.index(i2) == mod:
-                            a = 0
-                        else:
-                            a = temp[temp.index(i2) - 1 - mod]
-                        b = full
-                        if len(temp) - temp.index(i2) == 2 - mod:
-                            c = 0
-                        else:
-                            c = temp[temp.index(i2) + 2 - mod]
-#                        if hits.get(b, 0) == 0:
-#                            hits[b] = []
-                        if not a == 0:
-                            if not len(temp[temp.index(i2) - 1 - mod]) < 3:
-                                # we are not interested in clues shorter than 3 letters long
-                                #ab = '%i %i' % (a, b)
-                                if b in bigrams.keys():
-                                    if a in bigrams[b].keys():
-                                        bigrams[b][a][0] += 1
-                                    else:
-                                        bigrams[b][a] = [1,0]
-                                else:
-                                    bigrams[b] = {a:[1,0]}
-#                                    hits[b].append(a)
-                        if not c == 0:
-                            if not len(temp[temp.index(i2) + 2 - mod]) < 3:
-                                #bc = '%i %i' % (b, c)
-                                if b in bigrams.keys():
-                                    if c in bigrams[b].keys():
-                                        bigrams[b][c][1] += 1
-                                    else:
-                                        bigrams[b][c] = [0,1]
-                                else:
-                                    bigrams[b] = {c:[0,1]}
-#                                    hits[b].append(c)
+        ts=set(temp)
+        if len(ts & tls)>0 or len(ts & tlsw)>1:
+            add_bigram(temp,ts & (tls | tlsw))
+    
     
     return bigrams
 
