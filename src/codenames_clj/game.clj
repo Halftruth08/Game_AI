0abfe28a783497ea133ee9ed1c2585e98e7a0cf9
Kanwei Li <kanwei@gmail.com>
2019-09-01 16:05:13 -0400

    Refactor to use deps.edn

--- src/codenames_clj/game.clj
+++ src/codenames_clj/game.clj
@@ -1,56 +1,56 @@
 (ns codenames-clj.game
-  (:require   [clojure.java.io :as io]
-              [clojure.string :as string]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
 
 (def number-of-codenames 400)
 
-(def codenames-list-file (io/resource "wordslist.txt"))  
+(def codenames-list-file (io/resource "wordslist.txt"))
 
-(def codenames  (vec (string/split (slurp codenames-list-file) #"\n")))
+(def codenames (vec (string/split (slurp codenames-list-file) #"\n")))
 
 (defn entry-words
   [entry]
-  
+
   (flatten (list (key entry) (keys (val entry)))))
 
 (defn compact
   [codenames-list full-model]
   (for [item full-model
         :when (not (not-any? true? (for [word (entry-words item)] (contains? (apply hash-set codenames-list) word))))]
-    item))             
+    item))
 
 (defn incorporate-new-pair
   [word-graph [[word association] m-weight]]
-  
+
   (if (not-any? #(clojure.string/includes? word %) (list "-" " "))
     (reduce (fn [graph new-word]
-              
+
               (if (not-any? #(clojure.string/includes? new-word %) (list "(" " " word))
                 (update-in graph
-                  [word new-word]
-                  (fn [counter]
-                    ;(println weight)
-                    (if counter
-                      (+ m-weight counter)
-                      m-weight))) 
+                           [word new-word]
+                           (fn [counter]
+                             ;(println weight)
+                             (if counter
+                               (+ m-weight counter)
+                               m-weight)))
                 graph))
-      
-      word-graph
-      [association])
+
+            word-graph
+            [association])
     word-graph))
 
 (defn reverse-hash
   [model]
-  ;(doseq [x (seq (for [item model] (for [word (seq (val item))] [[(key word) (key item)] (val word)])))] 
+  ;(doseq [x (seq (for [item model] (for [word (seq (val item))] [[(key word) (key item)] (val word)])))]
   ;       (println x)]
   (reduce incorporate-new-pair
-    {}
-    (seq (for [item model
-               x (for [word (seq (val item))] [[(key word) (key item)] (val word)])]
-              x))))
-             
+          {}
+          (seq (for [item model
+                     x (for [word (seq (val item))] [[(key word) (key item)] (val word)])]
+                 x))))
 
-                    
+
+
 
 ;(def codenames (range 400))
 (defn get-codeword
@@ -63,19 +63,19 @@
   []
   (let [vv (repeat 25 number-of-codenames)]
     (vec (map rand-int vv))))
-    
- 
+
+
 (defn get-wordlist-words
   "Simply pull words from the list of codenames at random"
   [wl]
-  (vec (map get-codeword wl)))  
+  (vec (map get-codeword wl)))
 
 (defn generate-gameboard
   ""
-  [words])  
+  [words])
 
 
-  
+
 
 (defn show-gameboard
   "display the words on the board to the player"
@@ -88,6 +88,6 @@
 
 (defn make-clue
   [game-hash compact-model])
-  
+
 
 
---
+++ test/halftruth/codenames_test.clj
@@ -0,0 +1,7 @@
+(ns halftruth.codenames-test
+  (:require [clojure.test :refer :all]
+            [halftruth.codenames :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
